b0f87c9ff69b6b6ec2250a52c53687fc34fb63b5
Grzegorz Hardejewicz <ghardejewicz@gmail.com>
2021-12-19 13:28:21 +0100

    MAIN/SECTION: Edytuje skrypt JS, ukrycie mobile menu po kliknięciu w link

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
+	// funkcja dodająca cień do paska menu przy scroll
+
 	const nav = document.querySelector('nav');
 
 	function addShadow() {
-		if (window.scrollY >= 200) {
+		if (window.scrollY >= 100) {
 			nav.classList.add('nav-shadow');
 		} else {
 			nav.classList.remove('nav-shadow');
@@ -10,4 +12,20 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 
 	window.addEventListener('scroll', addShadow);
+
+	// funkcja ukrywająca mobile menu po kliknięciu na link z menu
+	const navbar = document.querySelector('.navbar-collapse')
+	const navLinkClick = document.querySelector('.navbar-nav');
+	// - alternatywne rozwiązanie
+	// const allNavItems = document.querySelectorAll('.nav-link')
+
+	// console.log(allNavItems);
+
+    // allNavItems.forEach(item => item.addEventListener('click', () => navbar.classList.remove('show')))
+
+	function removeMenu() {
+		navbar.classList.remove('show');
+	}
+
+	navLinkClick.addEventListener('click', removeMenu);
 });
