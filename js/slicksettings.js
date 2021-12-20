5cec10c11a1173f70d7878da9d0180d3d0a00556
Grzegorz Hardejewicz <ghardejewicz@gmail.com>
2021-12-20 15:25:22 +0100

    MAIN/SECTION5: ustawiam slicka do obsługi RWD w sekcji 'TEAM'; koryguje ustawienie scroll-spy

--- js/slicksettings.js
+++ js/slicksettings.js
@@ -1,5 +1,25 @@
 $('.team-carousel').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+	arrows: false,
+    autoplaySpeed: 3000,
+	autoplay: true,
+	mobileFirst: true,
+	pauseOnFocus: true,
+	// infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 2,
+				// slidesToScroll: 2,
+			},
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+			},
+		},
+	],
+});
